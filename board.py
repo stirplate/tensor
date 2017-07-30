cfae5240be36581a411d5ddd657b1169f56520e4
Keith Gonzales <keith@anotherMacbook.local>
2017-07-30 17:24:46 -0400

    testing

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 # Import MNIST data
-#old from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+#old mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 import tensorflow as tf
 
